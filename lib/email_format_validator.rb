a563abe1c7cf0854404eb38677ae060a7861922b
Axel Vergult <axel.vergult@gmail.com>
2011-11-09 15:20:14 +0100

    Email Pattern RFC 822 now works

--- lib/email_format_validator.rb
+++ lib/email_format_validator.rb
@@ -1,26 +1,25 @@
 # -*- encoding: utf-8 -*-
+require "uri"
 
 class EmailFormatValidator < ActiveModel::EachValidator
 
-  EmailAddress = begin
-    qtext = '[^\\x0d\\x22\\x5c\\x80-\\xff]'
-    dtext = '[^\\x0d\\x5b-\\x5d\\x80-\\xff]'
-    atom = '[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-' +
-      '\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+'
-    quoted_pair = '\\x5c[\\x00-\\x7f]'
-    domain_literal = "\\x5b(?:#{dtext}|#{quoted_pair})*\\x5d"
-    quoted_string = "\\x22(?:#{qtext}|#{quoted_pair})*\\x22"
-    domain_ref = atom
-    sub_domain = "(?:#{domain_ref}|#{domain_literal})"
-    word = "(?:#{atom}|#{quoted_string})"
-    domain = "#{sub_domain}(?:\\x2e#{sub_domain})*"
-    local_part = "#{word}(?:\\x2e#{word})*"
-    addr_spec = "#{local_part}\\x40#{domain}"
-    pattern = Regexp.new("\A#{addr_spec}\z", nil, 'n')
+  module Patterns
+    def self.create_regexp(string)
+      Regexp.new string, nil, 'n'
+    end
+    ATOM      = create_regexp "[^\\x00-\\x20\\x22\\x28\\x29\\x2c\\x2e\\x3a-\\x3c\\x3e\\x40\\x5b-\\x5d\\x7f-\\xff]+"
+    QTEXT     = create_regexp "[^\\x0d\\x22\\x5c\\x80-\\xff]"
+    QPAIR     = create_regexp "\\x5c[\\x00-\\x7f]"
+    QSTRING   = create_regexp "\\x22(?:#{QTEXT}|#{QPAIR})*\\x22"
+    WORD      = create_regexp "(?:#{ATOM}|#{QSTRING})"
+    LOCAL_PT  = create_regexp "#{WORD}(?:\\x2e#{WORD})*"
+    ADDRESS   = create_regexp "#{LOCAL_PT}\\x40#{URI::REGEXP::PATTERN::HOSTNAME}"
+
+    EMAIL    = /\A#{ADDRESS}\z/
   end
 
   def validate_each(record, attribute, value)
-    unless value =~ EmailAddress
+    unless value =~ Patterns::EMAIL
       record.errors[attribute] << (options[:message] || "is not formatted properly")
     end
   end
