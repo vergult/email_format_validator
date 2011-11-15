a844d09c00e7c346069405c5199d50cdf0c81eea
Axel Vergult <axel.vergult@gmail.com>
2011-11-15 17:16:16 +0100

    Refactoring

--- lib/email_format_validator.rb
+++ lib/email_format_validator.rb
@@ -23,13 +23,20 @@ class EmailFormatValidator < ActiveModel::EachValidator
   def validate_each(record, attribute, value)
     pattern_choosed = options[:rfc] ? Patterns::EMAIL : Patterns::LESS_RFC_COMPLIANT_EMAIL
     unless value =~ pattern_choosed
-      record.errors.add(
-        attribute,
-        options[:message] || I18n.t(:improperly_formatted,
-                                    :scope    => "#{record.class.i18n_scope}.errors.models.#{record.class.model_name.i18n_key}.attributes.#{attribute}",
-                                    :default => "is improperly formatted")
-      )
+      record.errors.add(attribute, options[:message] || invalid_message(record, attribute))
     end
+  end
+
+  #######################
+  ### Private methods ###
+  #######################
+
+  private
+
+  def invalid_message(record, attribute)
+    I18n.t  :improperly_formatted,
+            :scope    => "#{record.class.i18n_scope}.errors.models.#{record.class.model_name.i18n_key}.attributes.#{attribute}",
+            :default => "is improperly formatted"
   end
 
 end
